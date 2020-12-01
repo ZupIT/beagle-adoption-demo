705e5e0165a363a896b2c8a5fdca8b3eb9b9b434
Luis Eduardo <luis.oliveira@zup.com.br>
2020-12-01 16:02:25 -0300

    feature: implementation sharedTest

--- example/android/app/src/test/java/br/com/zup/beaglehackday/ExampleUnitTest.kt
+++ example/android/app/src/test/java/br/com/zup/beaglehackday/ExampleUnitTest.kt
@@ -22,9 +22,8 @@
 
 package br.com.zup.beaglehackday
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
